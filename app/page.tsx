23734047df3941e7cc6a04b5031086f0c4bcb4b2
staneby <stanislaunebyshyniets@gmail.com>
2024-09-19 16:53:33 +0300

    remove the inline repository name

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-center">
         <h2 className="text-xl">Asimsoft LLC</h2>
-        <Image src={`${prefix}/asimsoft_website/images/tools.svg`} width={50} height={50} alt="tool"/>
+        <Image src={`${prefix}/images/tools.svg`} width={50} height={50} alt="tool"/>
         <h1 className="text-2xl text-center">The website is currently under maintenance. Sorry for the inconvenience.</h1>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
